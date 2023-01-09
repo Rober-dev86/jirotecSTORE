740086823c34657b8b9cb4c7aa59586087d7eb8e
Rober <alaniarober@gmail.com>
2023-01-08 20:03:44 -0500

    commit segundo

---
+++ js/app.js
@@ -0,0 +1,79 @@
+//toggle menu/navbar script
+$(".menu-btn2").hide();
+$(".menu-btn1").click(function() {
+    $(".sombra").toggleClass("mostrar");
+    $(".navegacion").toggleClass("active");
+    $(".menu-btn1").hide();
+    $(".menu-btn2").show();
+});
+
+$(".menu-btn2").click(function() {
+    $(".sombra").toggleClass("mostrar");
+    $(".navegacion").toggleClass("active");
+    $(".menu-btn2").hide();
+    $(".menu-btn1").show();
+});
+
+const slider = document.querySelector("#slider"); //la constante llama al id slider
+let sliderSection = document.querySelectorAll(".slider__section"); //es una variable que llama a los slider section
+let sliderSectionLast = sliderSection[sliderSection.length - 1]; //obtener el ultimo slider para poner al principio
+
+const btnLeft = document.querySelector("#btn-left"); //constamtes fijos para bones
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast); //coloca la ultima imagen despues que inicia la clase SLIDER
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0]; //captura la primera imagen
+    slider.style.marginLeft = "-200%"; //avanza 2 imagenes para mostrar la tercera
+    slider.style.transition = "all 1s";
+
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst); //coloca la primera imagen al ultimo antes de terminar la clase SLIDER
+        slider.style.marginLeft = "-100%";
+    }, 1000); // 500 es medio segundo
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = "0"; //muestra la ultima imagen que se coloco despues que inicia la clase SLIDER
+    slider.style.transition = "all 1s";
+
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 1000);
+}
+
+btnRight.addEventListener("click", function() {
+    //escucha el evento click para ejecutar la funcion NEXT
+    Next();
+});
+
+btnLeft.addEventListener("click", function() {
+    //escucha el evento click para ejecutar la funcion PREV
+    Prev();
+});
+
+//funcion para avance automatico
+setInterval(function() {
+    Next();
+}, 4000);
+
+//accediendo a los enlaces con querySelectorAll
+
+const enlaces = document.querySelector("#social");
+enlaces.href = "https://www.facebook.com/jirotec";
+
+//submenu
+
+$(".submenu").click(function() {
+    $(this).children("ul").slideToggle();
+});
+
+$("ul").click(function(propagacion) {
+    propagacion.stopPropagation();
+});
